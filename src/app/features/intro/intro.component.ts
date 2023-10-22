e99ebfcc6d9a8dae6082a87b9ce0e7e4335da373
NinoChekurishvili <115742132+NinoChekurishvili@users.noreply.github.com>
2023-10-22 05:41:39 +0400

    brand

--- src/app/features/intro/intro.component.ts
+++ src/app/features/intro/intro.component.ts
@@ -1,11 +1,12 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from 'src/app/shared/button/button.component';
+import { ButtonComponent } from 'src/app/shared/components/button/button.component';
+import { BrandComponent } from 'src/app/shared/components/brand/brand.component';
 
 @Component({
   selector: 'app-intro',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, BrandComponent],
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ src/app/shared/components/brand/brand.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-brand',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './brand.component.html',
+  styleUrls: ['./brand.component.scss']
+})
+export class BrandComponent {
+
+}
